a689f5f7491b52697754c00a5f42b373316e9a58
tlokotsi0 <amantusf@gmail.com>
2026-09-05 11:46:21 +0200

    Dynamic Arrays assignment

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -9,11 +9,14 @@ public static class Arrays
     public static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        double[] result = new double[length];
 
-        return []; // replace this return statement with your own
+        for (int i = 0; i<length; i++)
+        {
+            result[i] = number * (i+1);
+        }
+
+        return result;
     }
 
     /// <summary>
@@ -26,8 +29,15 @@ public static class Arrays
     public static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        if (data == null || data.Count <= 1 || amount % data.Count == 0)
+        {
+            return;
+        }
+
+        int splitIndex = data.Count - amount;
+        List<int> tailSegment = data.GetRange(splitIndex, amount);
+        data.RemoveRange(splitIndex, amount);
+        data.InsertRange(0, tailSegment);
+
     }
 }
